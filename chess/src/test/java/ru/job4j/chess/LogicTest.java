43dc29b59f70f851f4eda8a325b21d6a51cf1120
Den <dezer1993@yandex.ru>
2022-02-19 21:52:11 +0300

    2. Каркас шахматной доски [#792 #263023]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -3,6 +3,7 @@ package ru.job4j.chess;
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
+import ru.job4j.chess.firuges.black.QueenBlack;
 
 public class LogicTest {
 
@@ -13,5 +14,19 @@ public class LogicTest {
         logic.add(black);
         black.way(Cell.H5);
         logic.move(Cell.C1, Cell.H5);
+    }
+
+    @Test(expected = FigureNotFoundException.class)
+    public void whenFigureNotFound() throws OccupiedCellException, FigureNotFoundException {
+        Logic logic = new Logic();
+        logic.move(Cell.A1, Cell.F3);
+    }
+
+    @Test(expected = OccupiedCellException.class)
+    public void whenFigureNotFree() throws FigureNotFoundException, OccupiedCellException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C1));
+        logic.add(new QueenBlack(Cell.E3));
+        logic.move(Cell.C1, Cell.G5);
     }
 }
